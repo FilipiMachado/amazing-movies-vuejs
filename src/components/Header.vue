87f5ecdd62108ab9b55391cf2fca7b1d605676c5
filipi <filipisee@hotmail.com>
2022-04-19 22:34:48 -0300

    structuring the project

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Header Component</div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {}
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
