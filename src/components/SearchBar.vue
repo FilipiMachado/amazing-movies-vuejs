812b20175f54b2fb314eced8355440127bbe8a4e
filipi <filipisee@hotmail.com>
2022-04-21 15:15:21 -0300

    styling search bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>SearchBar Component</div>
+  <div class="searchbar__container">
+    <div class="searchbar__wrapper">
+      <span class="material-symbols-outlined searchbar__icon">
+        search
+      </span>
+      <input class="searchbar__input" type="text">
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,5 +19,19 @@ export default {
 </script>
 
 <style scoped>
-
+.searchbar__container {
+  display: flex;
+  align-items: center;
+  height: 100px;
+  padding: 0px 30px;
+  background-color: #1c1c1c;
+}
+.searchbar__wrapper {
+  display: flex;
+  align-items: center;
+}
+.searchbar__icon {
+  cursor: pointer;
+}
+.searchbar__input {}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <HeroImage />
+    <SearchBar />
   </div>
 </template>
 
 <script>
 import HeroImage from '../components/HeroImage.vue'
+import SearchBar from '../components/SearchBar.vue'
 
 export default {
   name: 'HomeView',
   components: {
     HeroImage,
+    SearchBar,
   },
   data() {
     return {}
