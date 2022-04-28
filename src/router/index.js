13537d60ae96e67344c194eeb88e2813c95badc3
filipi <filipisee@hotmail.com>
2022-04-27 21:26:44 -0300

    testing routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import MoviesView from '../views/MoviesView.vue'
+import MoviesInfo from '../components/MoviesInfo.vue'
 
 const routes = [
   {
@@ -11,7 +11,7 @@ const routes = [
   {
     path: '/moviesinfo',
     name: 'moviesinfo',
-    component: MoviesView
+    component: MoviesInfo
   }
 ]
 
