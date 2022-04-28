d687195f22c1149de86227c88bdc2e33940f0c7d
filipi <filipisee@hotmail.com>
2022-04-27 22:18:53 -0300

    structuring movies info component

--- src/components/MoviesInfo.vue
+++ src/components/MoviesInfo.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>Movies Info</div>
+  <div>
+    <div class="moviesinfo__main-content">
+      <div class="moviesinfo__left-content">
+        <img class="moviesinfo__left-content__img" src="../assets/no-image-available.png" alt="">
+      </div>
+      <div class="moviesinfo__right-content">
+        <div class="moviesinfo__right-content__title-wrapper">
+          <span class="moviesinfo__right-content__movie-title">The Batman</span>
+          <span class="moviesinfo__right-content__movie-sinopsis">SINOPSE</span>
+          <span class="moviesinfo__right-content__movie-description">In his second year of fighting crime, Batman uncovers corruption in Gotham City that connects to his own family while facing a serial killer known as the Riddler.</span>
+        </div>
+        <div>
+          <div>
+            <span>RATING</span>
+            <div>7.8</div>
+          </div>
+          <div>
+            <span>Director</span>
+            <div>Matt Reeves</div>
+        </div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,5 +36,7 @@ export default {
 </script>
 
 <style scoped>
-
+img {
+  width: 500px;
+}
 </style>
